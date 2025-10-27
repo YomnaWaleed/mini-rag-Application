d694b0b87e56683310a24853e38076a2812e7312
LAPSHOP <yomnawaleed2022@gmail.com>
2025-10-27 19:47:43 +0300

    seven

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -5,8 +5,12 @@ class Settings(BaseSettings):
     APP_NAME: str = "mini-RAG"
     APP_VERSION: str = "0.1"
     OPENROUTER_API_KEY: str
+    FILE_ALLOWED_TYPES: list
+    FILE_MAX_SIZE: int
+    FILE_DEFAULT_CHUNK_SIZE: int
 
-    model_config = SettingsConfigDict(env_file=".env")
+    class Config:
+        env_file = ".env"
 
 
 def get_settings():
--- src/main.py
+++ src/main.py
@@ -13,10 +13,9 @@ print(llm("what is the capital of France?"))
 """
 
 from fastapi import FastAPI
-
-
-from routes import base
+from routes import base, data
 
 
 app = FastAPI()
 app.include_router(base.base_router)
+app.include_router(data.data_router)
