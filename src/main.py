f97e45535acb2c5179c5bb79a66bc02e77fd87d1
LAPSHOP <yomnawaleed2022@gmail.com>
2025-11-17 19:15:12 +0200

    video 15

--- src/main.py
+++ src/main.py
@@ -17,7 +17,7 @@ from routes import base, data
 from motor.motor_asyncio import AsyncIOMotorClient
 from helpers.config import get_settings
 from stores.llm.LLMProviderFactory import LLMProviderFactory
-
+from stores.vectordb.VectorDBProviderFactory import VectorDBProviderFactory
 
 app = FastAPI()
 
@@ -26,31 +26,31 @@ async def startup_db_client():
     settings = get_settings()
     app.mongo_conn = AsyncIOMotorClient(settings.MONGODB_URL)
     app.db_client = app.mongo_conn[settings.MONGODB_DATABASE]
-    
+
     llm_provider_factory = LLMProviderFactory(config=settings)
-    
-    #generation client 
+    vectordb_provider_factory = VectorDBProviderFactory(config=settings)
+
+    # generation client
     app.generation_client = llm_provider_factory.create(
-        provider = settings.GENERATION_BACKEND
+        provider=settings.GENERATION_BACKEND
     )
-    app.generation_client.set_generation_model(
-        model_id= settings.GENERATION_MODEL_ID
-    )
-    
+    app.generation_client.set_generation_model(model_id=settings.GENERATION_MODEL_ID)
+
     # embedding client
     app.embedding_client = llm_provider_factory.create(
-        provider = settings.EMBEDDING_BACKEND
+        provider=settings.EMBEDDING_BACKEND
     )
     app.embedding_client.set_embedding_model(
-        model_id= settings.EMBEDDING_MODEL_ID, 
-        embedding_size= settings.EMBEDDING_MODEL_SIZE 
+        model_id=settings.EMBEDDING_MODEL_ID,
+        embedding_size=settings.EMBEDDING_MODEL_SIZE,
     )
-    
-    
+
+    # vectordb client
 
 
 async def shutdown_db_client():
     app.mongo_conn.close()
+
 
 app.router.lifespan.on_startup.append(startup_db_client)
 app.router.lifespan.on_shutdown.append(shutdown_db_client)
