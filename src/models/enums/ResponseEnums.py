39c7775576529014580e816c44f7e789f68d5d0b
LAPSHOP <yomnawaleed2022@gmail.com>
2025-11-30 00:57:09 +0200

    video 17

--- src/models/enums/ResponseEnums.py
+++ src/models/enums/ResponseEnums.py
@@ -1,6 +1,5 @@
 from enum import Enum
 
-
 class ResponseSignal(Enum):
 
     FILE_VALIDATED_SUCCESS = "file_validate_successfully"
@@ -8,8 +7,8 @@ class ResponseSignal(Enum):
     FILE_SIZE_EXCEEDED = "file_size_exceeded"
     FILE_UPLOAD_SUCCESS = "file_upload_success"
     FILE_UPLOAD_FAILED = "file_upload_failed"
-    PROCESSING_FAILED = "prcessing_failed"
     PROCESSING_SUCCESS = "processing_success"
+    PROCESSING_FAILED = "processing_failed"
     NO_FILES_ERROR = "not_found_files"
     FILE_ID_ERROR = "no_file_found_with_this_id"
     PROJECT_NOT_FOUND_ERROR = "project_not_found"
@@ -18,3 +17,5 @@ class ResponseSignal(Enum):
     VECTORDB_COLLECTION_RETRIEVED = "vectordb_collection_retrieved"
     VECTORDB_SEARCH_ERROR = "vectordb_search_error"
     VECTORDB_SEARCH_SUCCESS = "vectordb_search_success"
+    RAG_ANSWER_ERROR = "rag_answer_error"
+    RAG_ANSWER_SUCCESS = "rag_answer_success"
